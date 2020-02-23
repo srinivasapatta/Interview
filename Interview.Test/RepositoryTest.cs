d81b2cd845ad243bf265b5c69a59d81e8e0b93db
srinivasa patta <psrinivasa@gmail.com>
2020-02-23 19:54:21 +0000

    Added test cases for Get method of the Repository

--- Interview.Test/RepositoryTest.cs
+++ Interview.Test/RepositoryTest.cs
@@ -28,5 +28,33 @@ namespace Interview.Test
 
             Assert.IsTrue((result.Contains(newItemToBeSaved)));
         }
+
+        [TestMethod]
+        public void Test_Get_ByExistingId_ReturnsExpectedItem()
+        {
+            var repository = new Repository<TestStorable<int>, int>();
+
+            repository.Save(new TestStorable<int> { Id = 1, Data = "FirstItem" });
+            repository.Save(new TestStorable<int> { Id = 2, Data = "SecondItem" });
+            repository.Save(new TestStorable<int> { Id = 3, Data = "ThirdItem" });
+
+            TestStorable<int> result = repository.Get(2);
+
+            Assert.AreEqual("SecondItem", result.Data);
+        }
+
+        [TestMethod]
+        public void Test_Get_NonExistingId_ReturnsNull()
+        {
+            var repository = new Repository<TestStorable<int>, int>();
+
+            repository.Save(new TestStorable<int> { Id = 1, Data = "FirstItem" });
+            repository.Save(new TestStorable<int> { Id = 2, Data = "SecondItem" });
+            repository.Save(new TestStorable<int> { Id = 3, Data = "ThirdItem" });
+
+            TestStorable<int> result = repository.Get(4);
+
+            Assert.AreEqual(null, result);
+        }
     }
 }
